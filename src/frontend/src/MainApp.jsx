0e664bc47432ec15a6d678635b563db57285b21c
Mitchel Humpherys <mitch@directangular.com>
2019-08-24 14:44:02 -0700

    ui: Super duper simple search

--- src/frontend/src/MainApp.jsx
+++ src/frontend/src/MainApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, withRouter } from "react-router-dom";
+import Autosuggest from 'react-autosuggest';
 import Home from './Home.jsx';
 import Explainer from './Explainer.jsx';
 import AddExplainer from './AddExplainer.jsx';
 import ManageApprovals from './ManageApprovals.jsx';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import storage from './storage.js';
 
 const ExplainerById = ({ match }) => {
@@ -38,34 +39,109 @@ const WeirdUrl = () => {
     return <h5>You appear to be lost, wandering in the wilderness. Go <a href="/">home</a>?</h5>;
 };
 
-const Nav = () => {
-    return (
-        <header className="py-3 yang-header mb-5 pr-3">
-          <div className="row flex-nowrap justify-content-between align-items-center">
-            <div className="col-4 pt-1"></div>
-            <div className="col-4 text-center">
-              <Link to="/">
-                <img style={{maxWidth: "100px", maxHeight: "100px"}} src="/static/yangexplains_logo.png" />
-              </Link>
+class SiteHeader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            suggestions: [],
+            value: '',
+        };
+
+        this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+        this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
+        this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
+    }
+
+    onSuggestionsFetchRequested({ value }) {
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+
+        if (inputLength === 0)
+            return [];
+
+        const suggestions = this.props.explainers.filter(
+            explainer => explainer.question.toLowerCase().indexOf(inputValue) !== -1
+        );
+
+        this.setState({suggestions});
+    }
+
+    onSuggestionsClearRequested() {
+        this.setState({suggestions: []});
+    }
+
+    onSearchChange(event, { newValue }) {
+        this.setState({value: newValue});
+    }
+
+    onSuggestionSelected(event, { suggestion }) {
+        this.props.history.push(`/q/${suggestion.id}`);
+        this.setState({value: ''});
+    }
+
+    render() {
+        const { value, suggestions } = this.state;
+
+        const inputProps = {
+            placeholder: "Search",
+            value,
+            onChange: this.onSearchChange,
+        };
+
+        return (
+            <div>
+              <header className="py-3 yang-header mb-5 pr-3">
+                <div className="row flex-nowrap justify-content-between align-items-center">
+                  <div className="col-4 pt-1"></div>
+                  <div className="col-4 text-center">
+                    <Link to="/">
+                      <img style={{maxWidth: "100px", maxHeight: "100px"}} src="/static/yangexplains_logo.png" />
+                    </Link>
+                  </div>
+                  <div className="col-4 d-flex justify-content-end align-items-center">
+                    {YangConfig.user ? <UserMenuLoggedIn /> : <UserMenuAnon />}
+                  </div>
+                </div>
+              </header>
+              <Autosuggest
+                  suggestions={suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  onSuggestionSelected={this.onSuggestionSelected}
+                  getSuggestionValue={explainer => explainer.question}
+                  renderSuggestion={explainer => <div>{explainer.question}</div> }
+                  inputProps={inputProps}
+              />
             </div>
-            <div className="col-4 d-flex justify-content-end align-items-center">
-              {YangConfig.user ? <UserMenuLoggedIn /> : <UserMenuAnon />}
-            </div>
-          </div>
-        </header>
-    );
-};
+        );
+    }
+}
+
+const SiteHeaderWithRouter = withRouter(SiteHeader);
 
 class MainApp extends React.Component {
     constructor(props) {
         super(props);
+
+        /* TODO: might actually be nice to have Redux in here :thinking: */
+        this.state = {
+            explainers: [],
+            suggestions: [],
+        };
+    }
+
+    async componentDidMount() {
+        const explainers = (await storage.getAllExplainers()).questions;
+        this.setState({explainers});
     }
 
     render() {
         return (
             <div>
               <Router>
-                <Nav />
+                <SiteHeaderWithRouter explainers={this.state.explainers} />
                 <div className="row">
                   <div className="col-6 offset-3">
                     <Switch>
