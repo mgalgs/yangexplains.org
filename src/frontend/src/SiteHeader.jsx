c69192ce5a501a29efed779dc1887ad6315d234b
Mitchel Humpherys <mitch@directangular.com>
2019-08-26 11:04:14 -0700

    ui: Toggle user menu on small screens

--- src/frontend/src/SiteHeader.jsx
+++ src/frontend/src/SiteHeader.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 const UserMenuLoggedIn = () => {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item dropdown">
