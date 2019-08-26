ab65957d7bd4f5522f8d1bc4d8e8323cbafb31ae
Mitchel Humpherys <mitch@directangular.com>
2019-08-26 10:58:48 -0700

    ui: Better responsive layout

--- src/frontend/src/SiteHeader.jsx
+++ src/frontend/src/SiteHeader.jsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 
 const UserMenuLoggedIn = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <nav className="navbar navbar-expand navbar-light bg-light">
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item dropdown">
@@ -36,14 +33,20 @@ const SiteHeader = () => {
         <div>
           <header className="py-3 yang-header mb-5 pr-3">
             <div className="row flex-nowrap justify-content-between align-items-center">
-              <div className="col-4 pt-1"></div>
-              <div className="col-4 text-center">
+              <div className="col d-none d-md-block pt-1"></div>
+              <div className="col d-none d-md-block text-center">
                 <Link to="/" className="yang-center-header">
                   <span className="bg-blue-brand">Yang</span>
                   <span className="bg-red-brand">Explains!</span>
                 </Link>
               </div>
-              <div className="col-4 d-flex justify-content-end align-items-center">
+              <div className="col d-md-none">
+                <Link to="/" className="yang-center-header">
+                  <span className="bg-blue-brand">Yang</span>
+                  <span className="bg-red-brand">Explains!</span>
+                </Link>
+              </div>
+              <div className="col d-flex justify-content-end align-items-center">
                 {YangConfig.user ? <UserMenuLoggedIn /> : <UserMenuAnon />}
               </div>
             </div>
