9f6514188564176ab5f98ca22e4f6d39218bb051
Mitchel Humpherys <mitch@directangular.com>
2019-08-25 19:33:04 -0700

    Revert "web: Some more URL handling"
    This reverts commit 1e284a8bce6a7dde591be7cec3c7083b1e1f888c.

--- src/backend/yangify/app.py
+++ src/backend/yangify/app.py
@@ -155,15 +155,10 @@ def login():
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    base_url = app.config.get('SITE_BASE_URL')
-    if base_url:
-        redirect_url += '/login/callback'
-    else:
-        redirect_url = request.base_url + "/callback"
-
+    base_url = app.config.get('SITE_BASE_URL') or request.base_url
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=redirect_url,
+        redirect_uri=base_url + "/callback",
         scope=["openid", "email", "profile"],
     )
     return redirect(request_uri)
