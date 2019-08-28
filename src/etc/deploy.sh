5edf114da5949b160791036f1e23e66a1b927888
Mitchel Humpherys <mitch@directangular.com>
2019-08-28 15:14:04 -0700

    etc: deploy.sh: pip install as root

--- src/etc/deploy.sh
+++ src/etc/deploy.sh
@@ -4,7 +4,7 @@ set -e
 cd ~/sites/yangexplains.org/src
 
 git pull --stat
-./composectl exec flaskapp sh -c "cd /app; pip install -r requirements.txt"
+docker exec --user=root src_flaskapp_1 sh -c "cd /app; pip install -r requirements.txt"
 ./composectl exec flaskapp sh -c "cd /app/yangify; flask db upgrade"
 ./composectl run --rm frontend sh -c "cd /code; npm install && npm run build"
 ./etc/collectstatic.sh
