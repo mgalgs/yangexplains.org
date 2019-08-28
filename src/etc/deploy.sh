13733c4b9bc650bfafe9d0998030746aabdb8278
Mitchel Humpherys <mitch@directangular.com>
2019-08-28 15:13:15 -0700

    etc: deploy.sh: Add pip install

--- src/etc/deploy.sh
+++ src/etc/deploy.sh
@@ -4,6 +4,7 @@ set -e
 cd ~/sites/yangexplains.org/src
 
 git pull --stat
+./composectl exec flaskapp sh -c "cd /app; pip install -r requirements.txt"
 ./composectl exec flaskapp sh -c "cd /app/yangify; flask db upgrade"
 ./composectl run --rm frontend sh -c "cd /code; npm install && npm run build"
 ./etc/collectstatic.sh
