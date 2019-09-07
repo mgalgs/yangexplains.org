f633e520dd1b122e0ed06cfa4e29eff0b458ca69
Mitchel Humpherys <mitch@directangular.com>
2019-09-07 10:17:41 -0700

    ui: TagsList: Add prop to limit number of tags

--- src/frontend/src/TagsList.jsx
+++ src/frontend/src/TagsList.jsx
@@ -8,20 +8,50 @@ import urls from './urls.js';
 class TagsList extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            showAll: false,
+        };
+
+        this.onExpandoClick = this.onExpandoClick.bind(this);
+    }
+
+    onExpandoClick(e) {
+        e.preventDefault();
+        this.setState({showAll: true});
     }
 
     render() {
-        const classes = this.props.subdued
+        const { maxTags, tags, subdued } = this.props;
+
+        const classes = subdued
                       ? "badge badge-pill badge-light"
                       : "badge badge-pill badge-info";
+
+        let displayTags, showExpando;
+        if (this.state.showAll || maxTags >= tags.length) {
+            displayTags = tags;
+            showExpando = false;
+        } else {
+            displayTags = tags.slice(0, maxTags);
+            showExpando = true;
+        }
+
         return (
             <span>
-              {this.props.tags.map(tag => (
+              {displayTags.map(tag => (
                   <Link key={tag.id}
                         to={urls.pretty.tag(tag)}
                         style={{margin: "0 2px"}}
                         className={classes}>{tag.text}</Link>
               ))}
+              {showExpando &&
+               <a href="#"
+                  onClick={this.onExpandoClick}
+                  className="ml-1 text-muted">
+                 <small>More</small>
+               </a>
+              }
             </span>
         );
     }
@@ -30,10 +60,12 @@ class TagsList extends React.Component {
 TagsList.propTypes = {
     tags: PropTypes.arrayOf(tagShape).isRequired,
     subdued: PropTypes.bool,
+    maxTags: PropTypes.number,
 };
 
 TagsList.defaultProps = {
     subdued: false,
+    maxTags: 10,
 };
 
 export default TagsList;
